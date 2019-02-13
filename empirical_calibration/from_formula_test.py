128f0d9e2c6a75f236d68f6ea2a6f9ec20ad858e
Jingang Miao <jmiao@google.com>
2019-02-13 12:24:44 -0500

    Add tests for from_formula api.
    PiperOrigin-RevId: 233768076

--- empirical_calibration/from_formula_test.py
+++ empirical_calibration/from_formula_test.py
@@ -12,82 +12,110 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""Formular API for empirical balancing."""
+"""Tests for from_formula."""
 
 from __future__ import absolute_import
 from __future__ import division
-from __future__ import google_type_annotations
 from __future__ import print_function
 
-# Dependency imports
 from empirical_calibration import empirical_calibration as ec
+from empirical_calibration import from_formula as fec
+
 import numpy as np
 import pandas as pd
+import pandas.util.testing as pd_testing
 import patsy
-from typing import Text, Tuple
+import unittest
+from absl.testing import parameterized
 
 
-def _dmatrix_from_formula(formula: Text,
-                          df: pd.DataFrame) -> pd.DataFrame:
-  """Generates dmatrix from formula and dataframe.
 
-  This is a wrapper around patsy's dmatrix function.
+class FromFormulaTest(parameterized.TestCase):
 
-  Args:
-    formula: Formula to be fed into patsy. No outcome variable allowed.
-    df: Df containing the raw data.
+  def setUp(self):
+    # Toy df and dmatrix for tests.
+    self.seed_df = pd.DataFrame({
+        "x": ["a", "b", "c"],
+        "y": [1, 2, 3]
+    }, columns=["x", "y"])
+    self.seed_dmatrix = pd.DataFrame({
+        "x[T.b]": [0.0, 1.0, 0.0],
+        "x[T.c]": [0.0, 0.0, 1.0],
+        "y": [1.0, 2.0, 3.0],
+        "x[T.b]:y": [0.0, 2.0, 0.0],
+        "x[T.c]:y": [0.0, 0.0, 3.0]
+    }, columns=["x[T.b]", "x[T.c]",
+                "y", "x[T.b]:y", "x[T.c]:y"])
+    # Generate df's and dmatrix's with more rows.
+    self.idx = [0, 0,
+                1, 1, 1, 1,
+                2, 2, 2, 2, 2, 2, 2, 2]
+    self.target_idx = [0, 0, 0, 0,
+                       1, 1, 1, 1,
+                       2, 2, 2, 2]
 
-  Returns:
-    Design matrix.
-  """
-  dmatrix = patsy.highlevel.dmatrix(formula, df, return_type="dataframe")
-  if "Intercept" in dmatrix.columns:
-    dmatrix.drop(columns="Intercept", inplace=True)
-  return dmatrix
+    self.df = self.seed_df.iloc[self.idx]
+    self.target_df = self.seed_df.iloc[self.target_idx]
+
+    # Drop interaction terms.
+    self.dmatrix = self.seed_dmatrix.iloc[self.idx]
+    self.target_dmatrix = self.seed_dmatrix.iloc[self.target_idx]
+
+    self.columns = ["x[T.b]", "x[T.c]", "y"]
+    self.dmatrix = self.dmatrix[self.columns]
+    self.target_dmatrix = self.target_dmatrix[self.columns]
+
+  def test_dmatrix_from_formula_drop_intercept(self):
+    # Intercept is dropped even when formula asks for Intercept.
+    self.assertNotIn(
+        "Intercept",
+        fec._dmatrix_from_formula(formula="~ 1 + x + y + x:y",
+                                  df=self.seed_df).columns)
+
+  def test_dmatrix_from_formula_with_interaction(self):
+    # x:y in formula asks for interaction.
+    pd_testing.assert_frame_equal(
+        self.seed_dmatrix,
+        fec._dmatrix_from_formula(formula="~ x + y + x:y", df=self.seed_df))
+
+  def test_dmatrix_from_formula_no_interaction(self):
+    # No interaction, so only main effect terms are expected.
+    pd_testing.assert_frame_equal(
+        self.seed_dmatrix[["x[T.b]", "x[T.c]", "y"]],
+        fec._dmatrix_from_formula(formula="~ x + y", df=self.seed_df))
+
+  def test_dmatrix_from_formula_y_raises_error(self):
+    # Error should be raised if dependent variable is specified in formula.
+    with self.assertRaises(patsy.PatsyError):
+      fec._dmatrix_from_formula(formula="y ~ x", df=self.seed_df)
+
+  @parameterized.named_parameters(
+      ("entropy, same weights", ec.Objective.ENTROPY, [1] * 12),
+      ("quadratic, same weights", ec.Objective.QUADRATIC, [1] * 12),
+      ("entropy,  varying weights", ec.Objective.ENTROPY, [1] * 4 + [1] * 8),
+      ("quadratic, varying weights", ec.Objective.QUADRATIC, [1] * 4 + [1] * 8),
+  )
+  def test_from_formula(self, objective, target_weights):
+    # Two api should give the same results.
+    # _ec indicates the original empirical_calibration API.
+    weights_ec, l2_norm_ec = ec.maybe_exact_calibrate(
+        covariates=self.dmatrix,
+        target_covariates=self.target_dmatrix,
+        target_weights=target_weights,
+        objective=objective)
+
+    # _fec indicates empirical_calibration's formula API.
+    formula = "~ x + y"
+    weights_fec, l2_norm_fec = fec.from_formula(
+        formula=formula,
+        df=self.df,
+        target_df=self.target_df,
+        target_weights=target_weights,
+        objective=objective)
+
+    np.testing.assert_almost_equal(weights_ec, weights_fec, decimal=3)
+    self.assertAlmostEqual(l2_norm_ec, l2_norm_fec, places=2)
 
 
-def from_formula(formula: Text,
-                 df: pd.DataFrame,
-                 target_df: pd.DataFrame,
-                 target_weights: np.ndarray = None,
-                 autoscale: bool = False,
-                 objective: ec.Objective = ec.Objective.ENTROPY,
-                 max_weight: float = 1.0,
-                 increment: float = 0.001) -> Tuple[np.ndarray, float]:
-  """"Runs empirical calibration function from formula.
-
-  This is the formula API of the maybe_exact_calibrate function.
-
-  Args:
-    formula: Formula used to generate design matrix.
-      No outcome variable allowed.
-    df: Data to be calibrated.
-    target_df: Data containing the target.
-    target_weights: Weights for target_df.
-      If None, equal weights will be used.
-    autoscale: Whether to scale  covariates to [0, 1] and apply the same
-      scaling to target covariates. Setting it to True can help improve
-      numerical stability.
-    objective: The objective of the convex optimization problem.
-    max_weight: The upper bound on weights. Must be between uniform weight
-      (1 / _size) and 1.0.
-    increment: The increment of the search sequence.
-
-  Returns:
-    A tuple of (weights, l2_norm) where
-      weights: The weights for the  subjects. They should sum up to 1.
-      l2_norm: The L2 norm of the covariate balance constraint.
-  """
-  target_covariates = _dmatrix_from_formula(formula=formula,
-                                            df=target_df)
-  covariates = _dmatrix_from_formula(formula=formula,
-                                     df=df)
-
-  return ec.maybe_exact_calibrate(
-      covariates=covariates,
-      target_covariates=target_covariates,
-      target_weights=target_weights,
-      autoscale=autoscale,
-      objective=objective,
-      max_weight=max_weight,
-      increment=increment)
+if __name__ == "__main__":
+  googletest.main()
