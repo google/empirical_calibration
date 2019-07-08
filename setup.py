f03a862541e2d77b845d35818745a9a8e82f7af9
Jingang Miao <jmiao@google.com>
2019-07-08 12:47:22 -0400

    Merged commit includes the following changes: 256997847  by jmiao:
        Clean out some metadata.
    
    --
    256986652  by jmiao:
    
        Add suvery calibration example with simulated data.
    
    --
    256986608  by jmiao:
    
        Fix data/__init__.py.
    
    --
    
    PiperOrigin-RevId: 256997847

--- setup.py
+++ setup.py
@@ -35,4 +35,17 @@ setup(
         'six',
         'sklearn',
     ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
 )
