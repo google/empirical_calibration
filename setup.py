a64404c93dbf0fdc04deabb67610ae8fe06fb1d2
Jingang Miao <jmiao@google.com>
2019-07-09 16:50:36 -0400

    Add from . import data.
    PiperOrigin-RevId: 257264207

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     author_email='no-reply@google.com',
     url='https://github.com/google/empirical_calibration',
     license='Apache 2.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=["data"]),
     install_requires=[
         'absl-py',
         'numpy >= 1.11.1',
