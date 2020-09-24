ced707c2ec567484495dae57183081b10635cbde
Jingang Miao <9098862+miaojingang@users.noreply.github.com>
2020-09-23 21:54:29 -0400

    Update to support baseline_weights.
    Based on derivations by Emil Martayan.

--- empirical_calibration/core.py
+++ empirical_calibration/core.py
@@ -62,13 +62,13 @@ class Objective(enum.Enum):
 
 def calibrate(covariates: np.ndarray,
               target_covariates: np.ndarray,
+              baseline_weights: np.ndarray = None,
               target_weights: np.ndarray = None,
               autoscale: bool = False,
               objective: Objective = Objective.ENTROPY,
               max_weight: float = 1.0,
               l2_norm: float = 0) -> Tuple[np.ndarray, bool]:
   """Calibrates covariates toward target.
-
   It solves a constrained convex optimization problem that minimizes the
   variation of weights for units while achieving direct covariate
   balance. The weighted mean of covariates would match the simple mean
@@ -78,7 +78,6 @@ def calibrate(covariates: np.ndarray,
   to relax the covariate constraint by increasing the `l2_norm`, or use the
   `maybe_exact_calibrate` function to find a feasible solution with the smallest
   `l2_norm`.
-
   Only the first moment is matched between the input covariates and
   targert_covariates. To match higher moments or some transformation of the raw
   covariates, one needs to manually include them in the input. Similarly, to
@@ -86,7 +85,6 @@ def calibrate(covariates: np.ndarray,
   variables first. Zhao (2017) shows that the covariates to be balanced serve as
   the linear predictors in the induced propensity score model and the outcome
   regression model.
-
   There are two choices of the optimization objective: entropy of the weights
   (entropy balancing, or EB) and effective sample size implied by the weights
   (quadratic balancing, or QB). EB can be viewed as minimizing the
@@ -96,24 +94,19 @@ def calibrate(covariates: np.ndarray,
   functions for the weights (or the odds of propensity scores) - `exp(x)` for EB
   and `max(x, 0)` for QB. Therefore, EB weights are strictly positive; while QB
   weights can be zero and induce sparsity.
-
   #### Examples
-
   ```python
   # Entropy balancing, exactly match treatment and weighted control.
   weights, success = empirical_calibration.calibrate(
       covariates=control_covariates,
       target_convariates=treatment_covariates)
-
   # Estimate the average target effect on the treated (ATT) for an univariate
   # outcome.
   att = np.mean(treatment_outcome) - np.sum(control_outcome * weights)
-
   # Estimate ATT for a multivariate outcome.
   att = np.mean(
       treatment_outcome, axis=0) - np.matmul(control_outcome.T, weights)
   ```
-
   Args:
     covariates: covariates to be calibrated. All values must be numeric.
       For categorical values, the `from_formula` function is often more
@@ -122,6 +115,10 @@ def calibrate(covariates: np.ndarray,
       number of columns should match `covariates`. All values must be numeric.
       For categorical values, the `from_formula` function is often more
       convenient.
+    baseline_weights: baseline weights. For example, survey data may come
+      with weights already. If None, uniform baseline weights will be used.
+      If given, instead of coming up with weights close to uniform, the
+      function will try to come up with weights close to the baseline weights.
     target_weights: Weights for target_covariates. These are needed when the
       target_covariates themselves have weights. Its length must equal the
       number of rows in target_covariates. If None, equal weights will be used.
@@ -135,12 +132,10 @@ def calibrate(covariates: np.ndarray,
     l2_norm: The L2 norm of the covaraite balance constraint, i.e., the
       Euclidean distance between the weighted mean of covariates and the simple
       mean of target covaraites after balancing.
-
   Returns:
     A tuple of (weights, success) where
       weights: The weights for the subjects. They should sum up to 1.
       success: Whether the constrained optimization succeeds.
-
   Raises:
     ValueError: incompatible target and covariates or invalid balancing options.
   """
@@ -165,6 +160,11 @@ def calibrate(covariates: np.ndarray,
     raise ValueError("max_weight %f cannot be smaller than uniform weight %f" %
                      (max_weight, uniform_weight))
 
+  if baseline_weights is None:
+    baseline_weights = np.repeat(uniform_weight, num_samples)
+  else:
+    baseline_weights = baseline_weights / sum(baseline_weights) * num_samples
+
   z = np.hstack(
       (np.expand_dims(np.ones(num_samples), 1),
        covariates - np.average(target_covariates,
@@ -174,18 +174,26 @@ def calibrate(covariates: np.ndarray,
     # Running entropy balancing directly with a weight bound may fail due to bad
     # initial value for beta, so we first run it with a huge bound (1e8) to get
     # a good guess of beta.
-    weight_link = lambda x: np.exp(np.minimum(x, np.log(1e8)))
+    weight_link = lambda x: baseline_weights * np.exp(
+        np.minimum(x - 1, np.log(1e8)))
     beta_init = np.zeros(num_covariates + 1)
   elif objective == Objective.QUADRATIC:
-    weight_link = lambda x: np.clip(x, 0.0, max_weight)
+    weight_link = lambda x: np.clip(
+        x * baseline_weights + baseline_weights, 0.0, max_weight)
     # Solution of the dual problem without the non-negative weight constraint.
+    # beta_init = np.linalg.solve(
+    #     np.matmul(z.T, z), np.concatenate((np.ones(1),
+    #                                        np.zeros(num_covariates))))
     beta_init = np.linalg.solve(
-        np.matmul(z.T, z), np.concatenate((np.ones(1),
-                                           np.zeros(num_covariates))))
+        z.T @ np.diag(baseline_weights) @ z,
+        np.concatenate((np.ones(1), np.zeros(num_covariates))) -
+        z.T @ baseline_weights
+    )
   else:
     raise ValueError("unknown objective %s" % objective)
 
   def estimating_equation(beta):
+    # weights = weight_link(np.dot(z, beta))
     weights = weight_link(np.dot(z, beta))
 
     norm = np.linalg.norm(beta[1:])
@@ -206,7 +214,8 @@ def calibrate(covariates: np.ndarray,
   logging.info("Number of function calls: %d", info_dict["nfev"])
 
   if objective == Objective.ENTROPY and np.max(weights) > max_weight:
-    weight_link = lambda x: np.exp(np.minimum(x, np.log(max_weight)))
+    weight_link = lambda x: baseline_weights * np.exp(
+        np.minimum(x - 1, np.log(max_weight)))
 
     logging.info(
         "Running calibration with objective=%s, autoscale=%s, l2_norm=%s, "
@@ -226,20 +235,19 @@ def calibrate(covariates: np.ndarray,
 
 def maybe_exact_calibrate(covariates: np.ndarray,
                           target_covariates: np.ndarray,
+                          baseline_weights: np.ndarray = None,
                           target_weights: np.ndarray = None,
                           autoscale: bool = False,
                           objective: Objective = Objective.ENTROPY,
                           max_weight: float = 1.0,
                           increment: float = 0.001) -> Tuple[np.ndarray, float]:
   """Finds feasible weights with the tightest covariate balance constraint.
-
   It is possible that there is no feasible solution for the weighted mean of
   covariates to exactly match the mean of target covariates. In such case, one
   needs to relax the covariate balance constraint. This function searches on a
   sequence of constraints for the tightest one that yields a feasible solution.
   The search is efficiently implemented with a bracket phase followed by a zoom
   phase.
-
   Args:
     covariates: covariates to be calibrated. All values must be numeric.
       For categorical values, the `from_formula` function is often more
@@ -248,6 +256,10 @@ def maybe_exact_calibrate(covariates: np.ndarray,
       number of columns should match `covariates`. All values must be numeric.
       For categorical values, the `from_formula` function is often more
       convenient.
+    baseline_weights: baseline weights. For example, survey data may come
+      with weights already. If None, uniform baseline weights will be used.
+      If given, instead of coming up with weights close to uniform, the
+      function will try to come up with weights close to the baseline weights.
     target_weights: Weights for target_covariates. These are needed when the
       target_covariates itself has weights. Its length must equal the number of
       rows in target_covariates. If None, equal weights will be used.
@@ -258,7 +270,6 @@ def maybe_exact_calibrate(covariates: np.ndarray,
     max_weight: The upper bound on weights. Must between uniform weight
       (1 / number of rows in `covariates`) and 1.0.
     increment: The increment of the search sequence.
-
   Returns:
     A tuple of (weights, l2_norm) where
       weights: The weights for the subjects. They should sum up to 1.
@@ -277,6 +288,7 @@ def maybe_exact_calibrate(covariates: np.ndarray,
       weights, success = calibrate(
           covariates,
           target_covariates,
+          baseline_weights,
           target_weights,
           autoscale=False,
           objective=objective,
@@ -298,6 +310,7 @@ def maybe_exact_calibrate(covariates: np.ndarray,
     mid_weights, success = calibrate(
         covariates,
         target_covariates,
+        baseline_weights,
         target_weights,
         autoscale=False,
         objective=objective,
@@ -320,13 +333,10 @@ def maybe_exact_calibrate(covariates: np.ndarray,
 
 def dmatrix_from_formula(formula: str, df: pd.DataFrame) -> pd.DataFrame:
   """Generates dmatrix from formula and dataframe.
-
   This is a wrapper around patsy's dmatrix function.
-
   Args:
     formula: Formula to be fed into patsy. No outcome variable allowed.
     df: Df containing the raw data.
-
   Returns:
     Design matrix.
   """
@@ -339,20 +349,23 @@ def dmatrix_from_formula(formula: str, df: pd.DataFrame) -> pd.DataFrame:
 def from_formula(formula: str,
                  df: pd.DataFrame,
                  target_df: pd.DataFrame,
+                 baseline_weights: np.ndarray = None,
                  target_weights: np.ndarray = None,
                  autoscale: bool = False,
                  objective: Objective = Objective.ENTROPY,
                  max_weight: float = 1.0,
                  increment: float = 0.001) -> Tuple[np.ndarray, float]:
   """"Runs empirical calibration function from formula.
-
   This is the formula API of the maybe_exact_calibrate function.
-
   Args:
     formula: Formula used to generate design matrix.
       No outcome variable allowed.
     df: Data to be calibrated.
     target_df: Data containing the target.
+    baseline_weights: baseline weights. For example, survey data may come
+      with weights already. If None, uniform baseline weights will be used.
+      If given, instead of coming up with weights close to uniform, the
+      function will try to come up with weights close to the baseline weights.
     target_weights: Weights for target_df. These are needed when the
       target_df itself has weights. Its length must equal the number of
       rows in target_df. If None, equal weights will be used.
@@ -363,7 +376,6 @@ def from_formula(formula: str,
     max_weight: The upper bound on weights. Must between uniform weight
       (1 / number of rows in `df`) and 1.0.
     increment: The increment of the search sequence.
-
   Returns:
     A tuple of (weights, l2_norm) where
       weights: The weights for the subjects. They should sum up to 1.
@@ -375,6 +387,7 @@ def from_formula(formula: str,
   return maybe_exact_calibrate(
       covariates=covariates,
       target_covariates=target_covariates,
+      baseline_weights=baseline_weights,
       target_weights=target_weights,
       autoscale=autoscale,
       objective=objective,
